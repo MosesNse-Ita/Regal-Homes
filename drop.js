538e69f70be30542639a449d1f89158abf5bddde
MosesNse-Ita <okonitamoseskokoette@gmail.com>
2023-07-17 12:56:01 +0100

    done

--- drop.js
+++ drop.js
@@ -1,7 +1,7 @@
     /* When the user clicks on the button, 
       toggle between hiding and showing the dropdown content */
       function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementById("myDropdown").classList.toggle("drop");
       }
 
       // Close the dropdown if the user clicks outside of it
@@ -11,8 +11,8 @@
           var i;
           for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show")) {
-              openDropdown.classList.remove("show");
+            if (openDropdown.classList.contains("drop")) {
+              openDropdown.classList.remove("drop");
             }
           }
         }
